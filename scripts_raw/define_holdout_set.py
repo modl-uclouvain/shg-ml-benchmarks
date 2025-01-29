3f6c124dbf6537355ce859b4cafc1ed922c9b947
Victor Trinquet <victor.trinquet@uclouvain.be>
2025-01-29 11:59:15 +0100

    add option of validation set

--- scripts_raw/define_holdout_set.py
+++ scripts_raw/define_holdout_set.py
@@ -6,29 +6,43 @@ def main():
 
     # Equi-distribution per bin
     strategy = "distribution"
-    n_holdout=500
-    id_holdout = utils.get_holdout_set(
+    n_holdout=250
+    include_val = True
+    id_holdout, id_validation = utils.get_holdout_validation_set(
         n_holdout=n_holdout,
         strategy_holdout=strategy,
+        include_validation=include_val,
     )
 
     path_id = f"../data/holdout_id_{strategy}_{n_holdout}.json"
     if not Path(path_id).exists():
         with open(path_id, "w") as f:
             json.dump(id_holdout, f)
+    if include_val:
+        path_id = f"../data/validation_id_{strategy}_{n_holdout}.json"
+        if not Path(path_id).exists():
+            with open(path_id, "w") as f:
+                json.dump(id_validation, f)
 
     # Random sampling
     strategy = "random"
-    n_holdout=500
-    id_holdout = utils.get_holdout_set(
+    n_holdout=250
+    include_val = True
+    id_holdout, id_validation = utils.get_holdout_validation_set(
         n_holdout=n_holdout,
         strategy_holdout=strategy,
+        include_validation=include_val,
     )
 
     path_id = f"../data/holdout_id_{strategy}_{n_holdout}.json"
     if not Path(path_id).exists():
         with open(path_id, "w") as f:
             json.dump(id_holdout, f)
+    if include_val:
+        path_id = f"../data/validation_id_{strategy}_{n_holdout}.json"
+        if not Path(path_id).exists():
+            with open(path_id, "w") as f:
+                json.dump(id_validation, f)
 
 if __name__=="__main__":
     main()
