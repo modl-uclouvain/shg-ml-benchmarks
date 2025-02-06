fb8e211cd7f1c69c13fd297764da310a53a0c55b
Matthew Evans <git@ml-evs.science>
2025-02-06 10:58:16 +0000

    Add 'composition-spacegroup' and 'robocrys' structure input repr benchmarks

--- benchmarks/darwin-1.5/benchmark.py
+++ benchmarks/darwin-1.5/benchmark.py
@@ -12,6 +12,7 @@
 #    "transformers>=4.28.1",
 #    "wandb>=0.19.5",
 #    "accelerate >= 0.26",
+#    "robocrys >= 0.2.10",
 # ]
 # ///
 """Modified from https://github.com/MasterAI-EAM/Darwin/commit/d6961e30f58af6943e9b66798098a232fb7f6b42."""
@@ -20,8 +21,9 @@ import logging
 from enum import Enum
 from functools import partial
 from pathlib import Path
+from robocrys import StructureCondenser, StructureDescriber
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARNING)
 
 import torch
 from transformers import LlamaForCausalLM, LlamaTokenizer
@@ -29,13 +31,22 @@ from transformers import LlamaForCausalLM, LlamaTokenizer
 from shg_ml_benchmarks import run_benchmark
 from shg_ml_benchmarks.utils import SHG_BENCHMARK_SPLITS
 
+STRUCTURE_CONDENSER = StructureCondenser(
+    mineral_matcher=False, use_symmetry_equivalent_sites=True
+)
+STRUCTURE_DESCRIBER = StructureDescriber(
+    describe_components=True, describe_component_makeup=True
+)
+
+CONTEXT_WINDOW: int = 512
+"""This seems to be the maximum number of tokens that can be processed by the model at once."""
+
 
 class LLMInputStructureRepresentation(str, Enum):
     composition = "composition"
     composition_spacegroup = "composition_spacegroup"
     robocrystallographer = "robocrystallographer"
     cif = "cif"
-    composition_structure = "composition_structure"
 
 
 def generate_prompt(instruction, input=None):
@@ -56,7 +67,11 @@ def generate_prompt(instruction, input=None):
 
 def process_response(response):
     """Process response from Darwin repo."""
-    response = response.split("Response: ")[1].split("\n")[0]
+    try:
+        response = response.split("Response: ")[1].split("\n")[0]
+    except Exception:
+        logging.warning(f"Response could not be processed: {response=}")
+        response = None
     return response
 
 
@@ -74,6 +89,11 @@ def evaluate(
     """Evaluation method from Darwin repo."""
     prompt = generate_prompt(instruction, input)
     input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
+
+    if len(input_ids) > CONTEXT_WINDOW - 1:
+        logging.error("Input too long, clipping prompt: %s", prompt)
+        input_ids = input_ids[:CONTEXT_WINDOW - 1]
+
     generated_ids = model.generate(
         input_ids,
         max_new_tokens=max_new_tokens,
@@ -86,6 +106,7 @@ def evaluate(
     )
     response = tokenizer.decode(generated_ids[0])
     response = process_response(response)
+    torch.cuda.empty_cache()
     return response
 
 
@@ -96,6 +117,31 @@ def predict_fn(model, structure, task_instruction: str):
     """
     if model.input_structure_repr == LLMInputStructureRepresentation.composition:
         input_structure = structure.composition.reduced_formula
+    elif (
+        model.input_structure_repr
+        == LLMInputStructureRepresentation.composition_spacegroup
+    ):
+        input_structure = f"{structure.composition.reduced_formula} in {structure.get_space_group_info()[0]}"
+    elif (
+        model.input_structure_repr
+        == LLMInputStructureRepresentation.robocrystallographer
+    ):
+        try:
+            input_structure = STRUCTURE_DESCRIBER.describe(
+                STRUCTURE_CONDENSER.condense_structure(structure)
+            )
+        except Exception as exc:
+            logging.error(
+                "Failed default structure repr generation for input: %s. Error: %s",
+                structure.reduced_formula,
+                str(exc),
+            )
+            # Some structures fail symmetrisation
+            input_structure = STRUCTURE_DESCRIBER.describe(
+                StructureCondenser(
+                    mineral_matcher=False, use_symmetry_equivalent_sites=False
+                ).condense_structure(structure)
+            )
     else:
         raise NotImplementedError(
             f"Input structure representation {model.input_structure_repr} not yet implemented."
@@ -113,34 +159,47 @@ def predict_fn(model, structure, task_instruction: str):
     return response
 
 
+def load_model():
+    model_path = Path(__file__).parent / "model"
+    tokenizer = LlamaTokenizer.from_pretrained(model_path)
+
+    model = LlamaForCausalLM.from_pretrained(
+        model_path, load_in_8bit=False, torch_dtype=torch.float16, device_map="auto"
+    )
+
+    return model, tokenizer
+
+
 system_prompt: str | None = None
-task_instruction: str = "Given this description of a crystal structure, predict its second-harmonic generation coefficient in the Kurtz-Perry form."
 
 input_structure_repr: LLMInputStructureRepresentation = (
     LLMInputStructureRepresentation.composition
 )
 
-model_path = Path(__file__).parent / "model"
-tokenizer = LlamaTokenizer.from_pretrained(model_path)
+model, tokenizer = load_model()
 
-model = LlamaForCausalLM.from_pretrained(
-    model_path, load_in_8bit=False, torch_dtype=torch.float16, device_map="auto"
-)
+for input_structure_repr in LLMInputStructureRepresentation:
+    task_instruction: str = f"Given this description of a crystal structure ({input_structure_repr.value.replace('_', ' ')}), predict its second-harmonic generation coefficient in the Kurtz-Perry form."
+    for split in SHG_BENCHMARK_SPLITS:
 
-model.input_structure_repr = input_structure_repr
-model.in_context_learning = False
-model.label = "darwin-1.5"
-model.tags = f"darwin-1.5-{input_structure_repr.value}"
-if model.in_context_learning:
-    model.tags += "-icl"
-else:
-    model.tags += "-no-icl"
-
-for split in SHG_BENCHMARK_SPLITS:
-    logging.info("Running benchmark %s for split %s", model.tags, split)
-    run_benchmark(
-        model=model,
-        predict_fn=partial(predict_fn, task_instruction=task_instruction),
-        task=split,
-        train_fn=None,
-    )
+        model.input_structure_repr = input_structure_repr
+        model.in_context_learning = False
+        model.label = "darwin-1.5"
+        model.tags = f"darwin-1.5-{input_structure_repr.value}"
+        if model.in_context_learning:
+            model.tags += "-icl"
+        else:
+            model.tags += "-no-icl"
+        logging.info("Running benchmark %s for split %s", model.tags, split)
+        try:
+            run_benchmark(
+                model=model,
+                predict_fn=partial(predict_fn, task_instruction=task_instruction),
+                task=split,
+                train_fn=None,
+            )
+        except NotImplementedError:
+            logging.error(
+                "NotImplementedError for split %s, repr %s", split, input_structure_repr
+            )
+            continue
