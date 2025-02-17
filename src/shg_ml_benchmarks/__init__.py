427ff7ab3ef60ae0a3b0e892e4f29e4c420ebac8
Matthew Evans <git@ml-evs.science>
2025-02-17 15:44:12 +0000

    Prevent failures if model does not set meta

--- src/shg_ml_benchmarks/__init__.py
+++ src/shg_ml_benchmarks/__init__.py
@@ -7,6 +7,7 @@ from typing import Any
 import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
+import plotly.io as pio
 from pymatgen.core import Structure
 from scipy.stats import spearmanr
 from sklearn.metrics import r2_score
@@ -266,15 +267,15 @@ def run_benchmark(
     fig_parity_plot = visualize_predictions(predictions, holdout_df, target)
 
     # Compile results
-    if uncertainties != {}:
-        results = {
-            "predictions": predictions,
-            "uncertainties": uncertainties,
-            "metrics": metrics,
-        }
-    else:
-        results = {"predictions": predictions, "metrics": metrics}
-    if model.meta:
+    if not uncertainties:
+        uncertainties = None
+
+    results = {
+        "predictions": predictions,
+        "uncertainties": uncertainties,
+        "metrics": metrics,
+    }
+    if getattr(model, "meta", None):
         results["meta"] = model.meta
 
     if write_results:
@@ -288,7 +289,6 @@ def run_benchmark(
         fig_fname = "parity_plot_pred_true"
         figs_path = results_path.parent / f"{model_tags}_figures" / f"{fig_fname}"
         figs_path.parent.mkdir(parents=True, exist_ok=True)
-        import plotly.io as pio
 
         pio.kaleido.scope.mathjax = None  # To remove MathJax box in pdf
         fig_parity_plot.write_image(f"{str(figs_path)}.pdf")
