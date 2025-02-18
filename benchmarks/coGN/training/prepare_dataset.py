29556f3b26233a2fb1107edf1625056f9d33f997
Victor Trinquet <victor.trinquet@uclouvain.be>
2025-02-18 11:04:32 +0100

    linting + fix exclude matten model symlink

--- benchmarks/coGN/training/prepare_dataset.py
+++ benchmarks/coGN/training/prepare_dataset.py
@@ -1,8 +1,8 @@
+from shg_ml_benchmarks.utils import (
+    load_full,
+)
 
-from shg_ml_benchmarks.utils import SHG_BENCHMARK_SPLITS, load_holdout, load_train, load_full
-
-# 1. Load the full training 
+# 1. Load the full training
 
 df_full = load_full()
-for split in SHG_BENCHMARK_SPLITS:
-
+# for split in SHG_BENCHMARK_SPLITS:
