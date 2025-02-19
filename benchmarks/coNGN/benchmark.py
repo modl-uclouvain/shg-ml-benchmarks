2e3c1697fbc6b887726f41b321ffc8392995c187
Victor Trinquet <victor.trinquet@uclouvain.be>
2025-02-19 09:59:40 +0100

    coGN + coNGN dflt distribution_125

---
+++ benchmarks/coNGN/benchmark.py
@@ -0,0 +1,56 @@
+import logging
+from functools import partial
+from pathlib import Path
+from typing import Any
+
+import pandas as pd
+
+logging.basicConfig(level=logging.INFO)
+
+from shg_ml_benchmarks import run_benchmark
+from shg_ml_benchmarks.utils import SHG_BENCHMARK_SPLITS
+
+
+def predict_fn(
+    model: Any,
+    ids: list,
+    path_pred: str | Path,
+    structures: list | None = None,  # Needed for compatibility with the other models
+):
+    # Load the predictions
+    df_pred = pd.read_json(path_pred)
+    check_len = len(df_pred)
+    df_pred = df_pred.filter(ids, axis=0)
+    assert len(df_pred) == check_len, (
+        "The size of the predictions list was modified by the filtering on the ids given."
+    )
+    df_pred = df_pred.rename(columns={"predictions": "dKP_full_neum"})
+
+    return df_pred
+
+
+# Just to have the model.label accessible
+class Object:
+    pass
+
+
+model = Object()
+model.label = "coNGN"
+
+for split in SHG_BENCHMARK_SPLITS:
+    if (
+        split != "distribution_125"
+    ):  # TODO: REMOVE ONCE THE OTHER TEST SETS HAVE BEEN RUN
+        continue
+
+    logging.info("Running benchmark for split %s", split)
+
+    path_pred = split + "/results_distribution_125.json.gz"
+
+    run_benchmark(
+        model=model,
+        predict_fn=partial(predict_fn, path_pred=path_pred),
+        task=split,
+        train_fn=None,
+        predict_individually=False,
+    )
---
+++ benchmarks/coNGN/training/prepare_dataset.py
@@ -0,0 +1,56 @@
+"Write the structures of the dataset as cif files in dataset/cif_files and build a csv with the target"
+
+import os
+from pathlib import Path
+
+import pandas as pd
+from pymatgen.core.structure import Structure
+
+from shg_ml_benchmarks.utils import (
+    SHG_BENCHMARK_SPLITS,
+    load_full,
+    load_holdout,
+    load_train,
+)
+
+# Load the full dataset (unique only)
+df_full = load_full()
+
+# Create the dataset/cif_files dir if necessary
+os.makedirs("dataset/cif_files", exist_ok=True)
+
+# Iterate over the structures and write them as cif files
+for i, s in enumerate(df_full["structure_rot"]):
+    path_cif = f"dataset/cif_files/{i}.cif"
+    if not Path(path_cif).exists():
+        Structure.from_dict(s).to_file(filename=path_cif)
+
+# Create a dataframe with a new index, this new ID, the name of the cif file, the target,
+# the material_id, and in which subset (train, test, val) the material is depending on the split
+path_id_prop = "dataset/id_prop.csv"
+if not Path(path_id_prop).exists():
+    df_id_prop = pd.DataFrame(
+        data={
+            "ID": range(len(df_full)),
+            "file": [f"{i}.cif" for i in range(len(df_full))],
+            "dKP_full_neum": [d for d in df_full["dKP_full_neum"]],
+            "material_id": [i for i in df_full.index],
+        }
+    )
+
+    for split in SHG_BENCHMARK_SPLITS:
+        df_train = load_train(split)
+        df_test = load_holdout(split)
+
+        type_subset = []
+        for ir in df_full.index:
+            if ir in df_train.index.tolist():
+                type_subset.append("train")
+            elif ir in df_test.index.tolist():
+                type_subset.append("test")
+            else:
+                type_subset.append("val")
+
+        df_id_prop[split] = type_subset
+
+    df_id_prop.to_csv(path_id_prop)
